b7be839b37398d8d220b2edfba55ead77c2543a0
pravigya <pravigyajain5@gmail.com>
2023-08-12 08:04:54 +0530

    createbrowsersupabaseclient error fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
+import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
 import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { useState } from "react";
 
@@ -6,7 +6,7 @@ import AppLayout from "src/core/layouts/App";
 import "src/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  const [supabaseClient] = useState(() => createPagesBrowserClient());
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient());
   return (
     <SessionContextProvider
       supabaseClient={supabaseClient}
