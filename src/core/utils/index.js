a34c0e5914d131dddb9191c845329f633222987b
Pravigya Jain <77122170+pravigya@users.noreply.github.com>
2023-08-13 05:09:27 +0530

    Update index.js -- prod url

--- src/core/utils/index.js
+++ src/core/utils/index.js
@@ -1,5 +1,5 @@
 const production = process.env.NODE_ENV === "production";
 
 export const SITE_URL = production
-  ? "https://saas-place-njrm.vercel.app"
+  ? "https://saas-place.vercel.app"
   : "http://localhost:3000";
